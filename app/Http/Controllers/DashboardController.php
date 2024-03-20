2a3bee7b09f3477384c83de389652202e4eb923d
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2024-03-20 07:31:04 +0700

    change database migration in english

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Paket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        // $data = DB::table("paket")->get();
+        // $data = Paket::all();
+        // dd($data);
         return view("admin.layout.dashboard");
     }
 }
