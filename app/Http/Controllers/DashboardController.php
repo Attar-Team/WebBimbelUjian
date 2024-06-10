abe70ff934d0b944e10aea3b8b2a29659a0b87f9
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2024-06-10 15:16:24 +0700

    fixing: item dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Package;
 use App\Models\Paket;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,8 +17,21 @@ class DashboardController extends Controller
         // $data = DB::table("paket")->get();
         // $data = Paket::all();
         // dd($data);
+
+        $user = User::where("role", "user")->get()->count();
+        $package = Package::all()->count();
+        $pemesanan = Order::all()->count();
+        $pemasukan = Order::all()->sum("gross_amount");
+        $pemesananDays = Order::whereDate("date",date("Y-m-d"))->get()->count();
+        $pemasukanDays = Order::whereDate("date",date("Y-m-d"))->get()->sum('gross_amount');
         return view("admin.layout.dashboard",[
-            "title"=> "Dashboard"
+            "title"=> "Dashboard",
+            "user"=> $user,
+            "package"=> $package,
+            "pemesanan"=> $pemesanan,
+            "pemasukan"=> $pemasukan,
+            "pemesananDays"=> $pemesananDays,
+            "pemasukanDays"=> $pemasukanDays
         ]);
     }
 }
--- resources/views/admin/layout/dashboard.blade.php
+++ resources/views/admin/layout/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('admin.template.template-admin')
 @section('content')
+@php
+use App\Util\FormatRupiah;
+@endphp
 <div class="container-xxl flex-grow-1 container-p-y">
     <div class="row">
       <div class="col-lg-8 mb-4 order-0">
@@ -7,13 +10,12 @@
           <div class="d-flex align-items-end row">
             <div class="col-sm-7">
               <div class="card-body">
-                <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
+                <h5 class="card-title text-primary">Selamat Datang {{ Auth::user()->name }} 🎉</h5>
                 <p class="mb-4">
-                  You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                  your profile.
+                  Selamat melihat aktivitas pemesanan pada penjualan anda
                 </p>
 
-                <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                {{-- <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a> --}}
               </div>
             </div>
             <div class="col-sm-5 text-center text-sm-left">
@@ -32,16 +34,12 @@
       </div>
       <div class="col-lg-4 col-md-4 order-1">
         <div class="row">
-          <div class="col-lg-6 col-md-12 col-6 mb-4">
+          <div class=" col-12  col-6 mb-4">
             <div class="card">
               <div class="card-body">
                 <div class="card-title d-flex align-items-start justify-content-between">
                   <div class="avatar flex-shrink-0">
-                    <img
-                      src="/assets-admin/img/icons/unicons/chart-success.png"
-                      alt="chart success"
-                      class="rounded"
-                    />
+                    <img src="/assets-admin/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
                   </div>
                   <div class="dropdown">
                     <button
@@ -60,12 +58,46 @@
                   </div>
                 </div>
                 <span class="fw-semibold d-block mb-1">Pemasukan</span>
-                <h3 class="card-title mb-2">$12,628</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
+                <h3 class="card-title mb-2">{{ FormatRupiah::Rupiah($pemasukan) }}</h3>
+                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i>{{ FormatRupiah::Rupiah($pemasukanDays) }}</small>
               </div>
             </div>
           </div>
-          <div class="col-lg-6 col-md-12 col-6 mb-4">
+        
+        </div>
+      </div>
+      <!-- Total Revenue -->
+      <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
+        <div class="card">
+          <div class="card-body px-0">
+            <div class="tab-content p-0">
+              <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
+                <div class="d-flex p-4 pt-3">
+                  <div class="avatar flex-shrink-0 me-3">
+                    <img src="/assets-admin/img/icons/unicons/wallet.png" alt="User" />
+                  </div>
+                  <div>
+                    <h5 class="text-muted d-block">Grafik Penjualan</h5>
+                    {{-- <div class="d-flex align-items-center">
+                      <h6 class="mb-0 me-1">$459.10</h6>
+                      
+                    </div> --}}
+                  </div>
+                </div>
+                <div id="incomeChart"></div>
+                <div class="d-flex justify-content-center pt-4 gap-2">
+           
+       
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--/ Total Revenue -->
+      <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+        <div class="row">
+            <div class="col-12 col-6 mb-4">
             <div class="card">
               <div class="card-body">
                 <div class="card-title d-flex align-items-start justify-content-between">
@@ -94,102 +126,11 @@
                   </div>
                 </div>
                 <span>Pemesanan</span>
-                <h3 class="card-title text-nowrap mb-1">$4,679</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+                <h3 class="card-title text-nowrap mb-1">{{ $pemesanan }}</h3>
+                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i>{{ $pemesananDays }}</small>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <!-- Total Revenue -->
-      <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
-        <div class="card">
-          <div class="card-header">
-            <ul class="nav nav-pills" role="tablist">
-              <li class="nav-item">
-                <button
-                  type="button"
-                  class="nav-link active"
-                  role="tab"
-                  data-bs-toggle="tab"
-                  data-bs-target="#navs-tabs-line-card-income"
-                  aria-controls="navs-tabs-line-card-income"
-                  aria-selected="true"
-                >
-                  Income
-                </button>
-              </li>
-              <li class="nav-item">
-                <button 
-                type="button"
-                class="nav-link"
-                role="tab"
-                data-bs-toggle="tab"
-                data-bs-target="#navs-tabs-line-card-profit"
-                aria-controls="navs-tabs-line-card-profit"
-                aria-selected="true"
-                >Profit</button>
-              </li>
-            </ul>
-          </div>
-          <div class="card-body px-0">
-            <div class="tab-content p-0">
-              <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
-                <div class="d-flex p-4 pt-3">
-                  <div class="avatar flex-shrink-0 me-3">
-                    <img src="/assets-admin/img/icons/unicons/wallet.png" alt="User" />
-                  </div>
-                  <div>
-                    <small class="text-muted d-block">Total Balance</small>
-                    <div class="d-flex align-items-center">
-                      <h6 class="mb-0 me-1">$459.10</h6>
-                      <small class="text-success fw-semibold">
-                        <i class="bx bx-chevron-up"></i>
-                        42.9%
-                      </small>
-                    </div>
-                  </div>
-                </div>
-                <div id="incomeChart"></div>
-                <div class="d-flex justify-content-center pt-4 gap-2">
-           
-       
-                </div>
-              </div>
-              <div class="tab-pane fade show" id="navs-tabs-line-card-profit">
-                <div class="d-flex p-4 pt-3">
-                  <div class="avatar flex-shrink-0 me-3">
-                    <img src="/assets-admin/img/icons/unicons/wallet.png" alt="User" />
-                  </div>
-                  <div>
-                    <small class="text-muted d-block">Total Balance</small>
-                    <div class="d-flex align-items-center">
-                      <h6 class="mb-0 me-1">$459.10</h6>
-                      <small class="text-success fw-semibold">
-                        <i class="bx bx-chevron-up"></i>
-                        42.9%
-                      </small>
-                    </div>
-                  </div>
-                </div>
-                <div id="incomeChart"></div>
-                <div class="d-flex justify-content-center pt-4 gap-2">
-                  <div class="flex-shrink-0">
-                    <div id="expensesOfWeek"></div>
-                  </div>
-                  <div>
-                    <p class="mb-n1 mt-1">Expenses This Week</p>
-                    <small class="text-muted">$39 less than last week</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--/ Total Revenue -->
-      <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
-        <div class="row">
           <div class="col-6 mb-4">
             <div class="card">
               <div class="card-body">
@@ -214,9 +155,9 @@
                     </div>
                   </div>
                 </div>
-                <span class="d-block mb-1">Payments</span>
-                <h3 class="card-title text-nowrap mb-2">$2,456</h3>
-                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+                <span class="d-block mb-1">Paket</span>
+                <h3 class="card-title text-nowrap mb-2">{{ $package }}</h3>
+             
               </div>
             </div>
           </div>
@@ -225,7 +166,7 @@
               <div class="card-body">
                 <div class="card-title d-flex align-items-start justify-content-between">
                   <div class="avatar flex-shrink-0">
-                    <img src="/assets-admin/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
+                    <i class="rounded fa-solid fa-user" style="padding: 13px;background-color: rgb(87, 237, 237,0.4)"></i>
                   </div>
                   <div class="dropdown">
                     <button
@@ -244,35 +185,14 @@
                     </div>
                   </div>
                 </div>
-                <span class="fw-semibold d-block mb-1">Transactions</span>
-                <h3 class="card-title mb-2">$14,857</h3>
-                <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
+                <span class="fw-semibold d-block mb-1">User</span>
+                <h3 class="card-title mb-2">{{ $user }}</h3>
               </div>
             </div>
           </div>
           <!-- </div>
 <div class="row"> -->
-          <div class="col-12 mb-4">
-            <div class="card">
-              <div class="card-body">
-                <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
-                  <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
-                    <div class="card-title">
-                      <h5 class="text-nowrap mb-2">Profile Report</h5>
-                      <span class="badge bg-label-warning rounded-pill">Year 2021</span>
-                    </div>
-                    <div class="mt-sm-auto">
-                      <small class="text-success text-nowrap fw-semibold"
-                        ><i class="bx bx-chevron-up"></i> 68.2%</small
-                      >
-                      <h3 class="mb-0">$84,686k</h3>
-                    </div>
-                  </div>
-                  <div id="profileReportChart"></div>
-                </div>
-              </div>
-            </div>
-          </div>
+
         </div>
       </div>
     </div>
