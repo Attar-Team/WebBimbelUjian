2db0fd501abb6963dc1ab08ca0c761f47ed4b16b
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2024-06-23 20:11:42 +0700

    update: sidebar

--- resources/views/admin/component/sidebar.blade.php
+++ resources/views/admin/component/sidebar.blade.php
@@ -48,12 +48,6 @@
           <div data-i18n="Analytics">Soal</div>
         </a>
       </li>
-      <li class="menu-item">
-        <a href="/users" class="menu-link">
-          <i class="menu-icon tf-icons bx bx-home-circle"></i>
-          <div data-i18n="Analytics">Lihat Ujian</div>
-        </a>
-      </li>
       <li class="menu-header pb-0 small text-uppercase">
         <span class="menu-header-text">Order</span>
       </li>
